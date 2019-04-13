9afc6a60009efb6efee4ffd3b0355331d57f070a
liyang.liu <liyang.liu@student.kuleuven.be>
2019-04-13 15:04:52 +0200

    raw processing data into one file and deal with two bodies

--- raw_data_processing.py
+++ raw_data_processing.py
@@ -1,14 +1,24 @@
 import os
 import numpy as np
 import random
+import csv
 import torch
 from pip._vendor.distlib.compat import raw_input
 
-path = '/Users/liuliyang/Downloads/nturgb+d_skeletons/'
+path = '/Users/liuliyang/Downloads/cs_train/'
+
 
 data = []
 
-for filename in os.listdir(path):
+list_of_files = os.listdir(path)
+count_of_file = 0
+for filename in list_of_files:
+    # in case system files are read in
+    if len(filename) is not 29:
+        count_of_file += 1
+        print('{}/{} files reading completed'.format(count_of_file, len(list_of_files)))
+        continue
+
     file = open(path + filename, 'r')
     lines = file.readlines()
     num_frames = int(lines[0])
@@ -20,21 +30,42 @@ for filename in os.listdir(path):
         if num_bodies is 1:
             numbers = []
             for j in range(3, 28):
-                numbers.append(list(map(float, lines[j].split(' ')[:-1])))
+                numbers.append(list(map(float, lines[j].split(' ')[:3])))
             numbers = np.array(numbers)
             numbers = numbers.flatten()
             frames.append(numbers)
+
             for j in range(0, 28):
                 lines = np.delete(lines, 0)
         else:
-            for j in range(0, num_bodies*27+1):
+            # check which is main actor when having two skeletons detected
+            numbers = []
+            numbers1 = []
+            numbers2 = []
+            for j in range(3, 28):
+                numbers1.append(list(map(float, lines[j].split(' ')[:3])))
+            for j in range(30, 55):
+                numbers2.append(list(map(float, lines[j].split(' ')[:3])))
+
+            if sum(map(sum,numbers1)) >= sum(map(sum,numbers2)):
+                numbers = numbers1
+            else:
+                numbers = numbers2
+            numbers = np.array(numbers)
+            numbers = numbers.flatten()
+
+            frames.append(numbers)
+            for j in range(0, 55):
                 lines = np.delete(lines, 0)
         count += 1
+
     num_frames = len(frames)
 
     # choosing 20 frames out of 20 sub-sequences
     # skip if the file has fewer than 20 frames
     if num_frames < 20:
+        count_of_file += 1
+        print('{}/{} files reading completed'.format(count_of_file, len(list_of_files)))
         continue
     chosen_frames = []
     sub_length = int(num_frames / 20)
@@ -53,6 +84,15 @@ for filename in os.listdir(path):
         target = 1
     else:
         target = 0
-    data.append([torch.Tensor(chosen_frames), target])
-    # print(data)
-    raw_input()
+    chosen_frames = np.array(chosen_frames)
+    # chosen_frames = chosen_frames.reshape(chosen_frames.shape[0] * chosen_frames.shape[1], 1)
+    chosen_frames = chosen_frames.flatten()
+    with open('/Users/liuliyang/Downloads/cs_train_csv/data.csv', 'a') as csvFile:
+        writer = csv.writer(csvFile)
+        writer.writerow(chosen_frames)
+    csvFile.close()
+
+    count_of_file += 1
+    print('{}/{} files reading completed'.format(count_of_file, len(list_of_files)))
+
+    # raw_input()
