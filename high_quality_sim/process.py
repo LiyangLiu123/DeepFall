77f6a13db31bbfb4081e27b15817aa80d8e5eedb
LiyangLiu123 <49199532+LiyangLiu123@users.noreply.github.com>
2019-05-27 14:51:18 +0200

    Update process.py

--- high_quality_sim/process.py
+++ high_quality_sim/process.py
@@ -2,57 +2,94 @@ import os
 import json
 import numpy as np
 
+duplicate=49
 data = []
 label = []
 test = []
 test_label = []
 meta = np.load("meta.npy")
 
+seq_len=90
+
 for fall in range(1, 56):
 	for cam in range(1,6):
 		directory = "./falls_keys/Fall"+str(fall)+"_Cam"+str(cam)+".avi_keys/"
 		if os.path.isdir(directory):
 			frames = []
-			f_count=0
+			isfall=True
+
+			mid=meta[fall-1][1]*30
+			k=mid-seq_len/2
+			p=mid+seq_len/2
 			
-			for i in range(len(os.listdir(directory))):
-				filename = "Fall"+str(fall)+"_Cam"+str(cam)+"_00000000"+str(i).zfill(4)+"_keypoints.json"
-				f = open(directory+filename, 'r') 
-				datastore = json.load(f)
-				if datastore['people']:
-					frame = []
-					for j in range(0, 54, 3):
-						frame.append([datastore['people'][0]['pose_keypoints_2d'][j], datastore['people'][0]['pose_keypoints_2d'][j+1]])
-					#print frame
-					frames.append(frame)
-					f_count+=1
-				f.close()
-				if f_count==30:
-					f_count=0
+			while k>=0:	
+				for i in range(seq_len):
+					filename = "Fall"+str(fall)+"_Cam"+str(cam)+"_00000000"+str(k+i).zfill(4)+"_keypoints.json"
+					f = open(directory+filename, 'r') 
+					datastore = json.load(f)
+					if datastore['people']:
+						frame = []
+						for j in range(0, 54, 3):
+							frame.append([datastore['people'][0]['pose_keypoints_2d'][j], datastore['people'][0]['pose_keypoints_2d'][j+1]])
+						#print frame
+						frames.append(frame)
+					f.close()
+
+				if len(frames)!=0:
 					#print len(frames)
+					#if fall>44:
 					if cam==5:
 						test.append(frames)
-						if i < meta[fall-1][0]*30 or i > meta[fall-1][2]*30:
-							test_label.append(0)
-						else:
+						if isfall:
 							test_label.append(1)
-					else:
-						if i < meta[fall-1][0]*30 or i > meta[fall-1][2]*30:
-							label.append(0)
-							data.append(frames)
+							isfall=False
 						else:
-							for _ in range(50):
+							test_label.append(0)
+					else:
+						if isfall:
+							for _ in range(duplicate):
 								label.append(1)
 								data.append(frames)
+							isfall=False
+						else:
+							label.append(0)
+							data.append(frames)
 					frames=[]
+				k-=seq_len
+
+			while p<len(os.listdir(directory)):	
+				for i in range(min(seq_len,len(os.listdir(directory))-p)):
+					filename = "Fall"+str(fall)+"_Cam"+str(cam)+"_00000000"+str(p+i).zfill(4)+"_keypoints.json"
+					f = open(directory+filename, 'r') 
+					datastore = json.load(f)
+					if datastore['people']:
+						frame = []
+						for j in range(0, 54, 3):
+							frame.append([datastore['people'][0]['pose_keypoints_2d'][j], datastore['people'][0]['pose_keypoints_2d'][j+1]])
+						#print frame
+						frames.append(frame)
+					f.close()
+
+				if len(frames)!=0:
+					#uncomment 'if fall...' and comment 'if cam...' for cross-scenario
+                    			#cross-view if remain this way
+					#if fall>44:
+					if cam==5:
+						test.append(frames)
+						test_label.append(0)
+					else:
+						label.append(0)
+						data.append(frames)
+					frames=[]
+				p+=seq_len
 
 			print(directory+" finished")
 			#print(np.asarray(data).shape)
 			#print(np.asarray(label).shape)
 			#print label
 
-np.save('train_data.npy',np.asarray(data))
-np.save('train_label.npy',np.asarray(label))
-np.save('test_data.npy',np.asarray(test))
-np.save('test_label.npy',np.asarray(test_label))
+np.save('train_data_'+str(seq_len)+'.npy',np.asarray(data))
+np.save('train_label_'+str(seq_len)+'.npy',np.asarray(label))
+np.save('test_data_'+str(seq_len)+'.npy',np.asarray(test))
+np.save('test_label_'+str(seq_len)+'.npy',np.asarray(test_label))
 
