30ca59d062bf04d842f920fcd98d93b64f8c649c
LiyangLiu123 <49199532+LiyangLiu123@users.noreply.github.com>
2019-05-27 14:47:43 +0200

    Update gen_len.py

--- high_quality_sim/gen_len.py
+++ high_quality_sim/gen_len.py
@@ -1,10 +1,16 @@
 import numpy as np
 
-test = np.load("test_label.npy")
-test_len = len(test)*[30]
+seq_len=90
 
-train = np.load("train_label.npy")
-train_len = len(train)*[30]
+test = np.load("test_data_"+str(seq_len)+".npy")
+test_len = []
+for t in test:
+	test_len.append(len(t))
 
-np.save("test_len.npy", test_len)
-np.save("train_len.npy", train_len)
+train = np.load("train_data_"+str(seq_len)+".npy")
+train_len = []
+for y in train:
+	train_len.append(len(y))
+
+np.save('test_len_'+str(seq_len)+'.npy', test_len)
+np.save('train_len_'+str(seq_len)+'.npy', train_len)
